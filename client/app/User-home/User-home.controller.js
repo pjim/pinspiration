7fd0050bec001699d7eb049fa8630f0d6fb5825e
pjim <paul.k.james@googlemail.com>
2015-11-04 08:23:24 +0000

    clean up

--- client/app/User-home/User-home.controller.js
+++ client/app/User-home/User-home.controller.js
@@ -1,27 +1,28 @@
 'use strict';
 
 angular.module('pinspirationApp')
-  .controller('UserHomeCtrl', function ($scope,ngDialog,$http,Auth) {
-    $scope.upload = function(){
-        ngDialog.open({
-          template:'components/modal.html',
-           scope: $scope
+  .controller('UserHomeCtrl', function ($scope, ngDialog, $http, Auth) {
+    // $scope.upload = function(){
+    //     ngDialog.open({
+    //       template:'components/modal.html',
+    //        scope: $scope
+    //
+    //     });
+    // }
+        $scope.click = function (){
+            alert('clicked');
+        };
+        $scope.addPin = function (event) {
+            event.preventDefault();
+           console.log('event');
+            var pinTit = $scope.pinName;
+            var pinLin  = $scope.pinLink;
+            var useId = Auth.getCurrentUser()._id;
+            $http.post('/api/users/addPin', {
 
-        });
-    }
-    $scope.click = function(){
-      alert('clicked');
-    }
-   $scope.addPin = function(event){
-     console.log('event');
-     var pinTit = $scope.pinName;
-     var pinLin  = $scope.pinLink;
-     var useId = Auth.getCurrentUser()._id;
-     $http.post('/api/users/addPin', {
-
-       id:useId,
-       pinTitle:pinTit,
-       pinLink: pinLink ''
-     })
-   }
+                id: useId,
+                pinTitle: pinTit,
+                pinLink: pinLink
+            });
+        }
   });
